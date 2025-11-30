ae91592c87f4d063496495ffaf2525448b306383
Taslim Yusuf <Omotoyosi2210@gmail.com>
2025-11-30 11:10:30 +0100

    about done

--- src/app/main-component/main-component.ts
+++ src/app/main-component/main-component.ts
@@ -2,10 +2,11 @@ import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
 import { Hero } from '../hero/hero';
 import { Resources } from '../resources/resources';
+import { About } from '../about/about';
 
 @Component({
   selector: 'app-main-component',
-  imports: [Navbar, Hero, Resources],
+  imports: [Navbar, Hero, Resources, About],
   templateUrl: './main-component.html',
   styleUrl: './main-component.css',
 })
